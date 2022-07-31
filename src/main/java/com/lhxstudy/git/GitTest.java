6fef025af21ff4e82de6972a867055d3306f0203
Lhx <Lhx@qq.com>
2022-07-31 17:35:59 +0800

    second commit

--- src/main/java/com/lhxstudy/git/GitTest.java
+++ src/main/java/com/lhxstudy/git/GitTest.java
@@ -2,6 +2,8 @@ package com.lhxstudy.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+
     }
 }
